d490e152793afc7685dc5cbf21511bbe238f9d25
Md. Sabbir Ahamed <sabbiryan@gmail.com>
2016-10-02 19:06:23 +0600

    first unit test create commit

---
+++ MathTests/MathTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Math;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathTests
+{
+    [TestClass]
+    public class MathTest
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            //arrange statements 
+            int num1 = 2;
+            int num2 = 3;
+
+            //act
+            int sum = MyMath.Add(num1, num2);
+
+
+            //assert 
+            Assert.AreEqual(5, sum);
+        }
+    }
+}
